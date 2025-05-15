7153f064b78d177f7dd4147ebe48c59f25bed029
maiconsbotelho <maicon.sbotelho@hotmail.com>
2025-05-15 15:30:47 -0300

    refactor: hml

--- src/interface_ws/ws_link.ts
+++ src/interface_ws/ws_link.ts
@@ -1,4 +1,4 @@
 export const WS_BASE =
   process.env.NEXT_PUBLIC_API_BASE_URL === 'production'
     ? 'https://api.selletesmalteria.com.br'
-    : 'https://api.selletesmalteria.com.br'; // ou outro para homologação
+    : 'https://hml.api.selletesmalteria.com.br'; // ou outro para homologação
