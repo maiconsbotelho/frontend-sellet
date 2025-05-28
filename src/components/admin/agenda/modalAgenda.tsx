cb9542e815dd0930d87e135041112130be2223fc
Maicon Botelho <maicon.sbotelho@hotmail.com>
2025-05-28 10:22:08 -0300

    refactor: ajustado responsividade dos modais

--- src/components/admin/agenda/modalAgenda.tsx
+++ src/components/admin/agenda/modalAgenda.tsx
@@ -45,10 +45,10 @@ const ModalAgenda: React.FC<ModalAgendaProps> = ({
   if (!isOpen) return null;
 
   return (
-    <div className="absolute top-[80px] left-0 right-0 pb-[80px] bg-opacity-50 flex items-center justify-center z-20 overflow-y-auto">
+    <div className="fixed top-[80px] inset-0 pb-[80px] bg-black bg-opacity-50 flex items-center justify-center z-20 ">
       <form
         onSubmit={onSubmit}
-        className="bg-white p-6 rounded shadow-lg w-full max-w-lg text-black"
+        className="bg-white p-6 rounded shadow-lg w-full max-w-lg text-black max-h-[calc(100vh-160px)] overflow-y-auto"
       >
         <h2 className="text-xl font-semibold mb-4 text-[var(--accent)]">
           {mode === 'add' ? 'Novo Agendamento' : 'Editar Agendamento'}
